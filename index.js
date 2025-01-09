7fe7b005c11633b23456002ce3fafd7e70674cde
Daniyal Awan <danizia.awan@gmail.com>
2025-01-09 22:39:03 +0500

    return earnings from method

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-function maxProfit(time, targetProfit) {
+function maxProfit(time) {
     // Calculate the maximum number of each property that can be built within the time limit
     const maxTheatres = Math.floor(time / 5);
     const maxPubs = Math.floor(time / 4);
     const maxParks = Math.floor(time / 10);
 
     // Calculate the profit of each possible combination
-    let maxProfitCombination = { profit: 0, properties: "" };
+    let maxProfitCombination = { earnings: 0, properties: "" };
     for (let t = 0; t <= maxTheatres; t++) {
         for (let p = 0; p <= maxPubs; p++) {
             for (let c = 0; c <= maxParks; c++) {
                 const totalTime = t * 5 + p * 4 + c * 10;
                 if (totalTime <= time) {
-                    const profit = t * 1500 + p * 1000 + c * 3000;
-                    if (profit >= targetProfit && profit > maxProfitCombination.profit) {
+                    const earnings = t * 1500 + p * 1000 + c * 3000;
+                    if (earnings > maxProfitCombination.earnings) {
                         maxProfitCombination = {
-                            profit,
+                            earnings,
                             properties: `T: ${t} P: ${p} C: ${c}`,
                         };
                     }
@@ -23,11 +23,14 @@ function maxProfit(time, targetProfit) {
         }
     }
 
-    // Return the properties string if a combination that meets the target profit is found
-    return maxProfitCombination.properties || "No combination found";
+    // Return total earnings
+    return maxProfitCombination.earnings
 }
 
 // Example usage
-console.log(maxProfit(5, 1000));
-console.log(maxProfit(75, 20000));
-console.log(maxProfit(10, 10000));
+console.log(maxProfit(2));
+console.log(maxProfit(5));
+console.log(maxProfit(9));
+console.log(maxProfit(15));
+console.log(maxProfit(30));
+console.log(maxProfit(68));
